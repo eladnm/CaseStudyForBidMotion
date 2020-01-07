f19e83e9cfc15ce577f066587d695ba58e14e00a
Elad Nahum <eladnm07@gmail.com>
2020-01-07 10:36:59 +0200

    sorting and pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,56 +5,41 @@ import { DataSource } from '@angular/cdk/collections';
 import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 import { Observable } from 'rxjs';
 
-
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = "my-case-study";
   dataSource = new MatTableDataSource([]);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+
   constructor(private serviceCountries: ServiceCountries) {}
-  ngOnInit() {
+
+  fetchDataPromise() {
     this.serviceCountries.getListCountriesPromise().then(res => {
     this.displayedColumns = ['continentName', 'countryName', 'areaInSqKm', 'population'];
     this.dataSource = new MatTableDataSource(res.geonames);
       console.log(res.geonames);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+
+
     });
   }
-  fetchData() {
-    this.serviceCountries.getListCountries().subscribe((data: any) => {
-      this.serviceCountries = data;
-      console.log(this.serviceCountries);
-    });
-  }
-  fetchDataPromise() {
-    this.serviceCountries.getListCountrriesPromise();
-  }
-    ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  
+
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
+    getTotal() {
+    return this.country.map(t => t.population).reduce((acc, value) => acc + value, 0);
+  }
 }
 
-
-export class CountryDataSource extends DataSource<any> {
-         constructor(private serviceCountries: ServiceCountries) {
-           super();
-         }
-         connect(): Observable<Country[]> {
-           return this.serviceCountries.getListCountries();
-         }
-         disconnect() {}
-       }
 
 
 
