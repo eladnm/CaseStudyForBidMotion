2cb9fed374695ee6533b6bbd245ce833dbdcbeb5
Elad Nahum <eladnm07@gmail.com>
2020-01-07 14:21:13 +0200

    total fix for items per page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,ViewChild } from '@angular/core';
 import { ServiceCountries } from './shared/service';
 import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
+import { MatPaginator, MatSort, MatTableDataSource, PageEvent} from "@angular/material";
 
 @Component({
   selector: "app-root",
@@ -13,6 +13,8 @@ export class AppComponent {
    show = false;
 dataSource = new MatTableDataSource([]);
 displayedColumns = ['continentName', 'countryName', 'areaInSqKm', 'population'];
+pageSize = 5;
+pageIndex = 0;
 geonames = [];
 @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 @ViewChild(MatSort, { static: false }) sort: MatSort;
@@ -25,24 +27,30 @@ this.geonames = res.geonames;
 this.dataSource = new MatTableDataSource(res.geonames);
 this.dataSource.sort = this.sort;
 this.dataSource.paginator = this.paginator;
+this.itemsShowed = this.dataSource.filteredData.slice(0, 5);
+this.dataSource.paginator.page.subscribe((pageEvent: PageEvent) => {
+this.pageSize = pageEvent.pageSize;
+this.pageIndex = pageEvent.pageIndex;
+})
 });
 }
 
 
   applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
+    this.dataSource.filter  = filterValue.trim().toLowerCase(); // Datasource defaults to lowercase matches
   }
-  getTotalPop() {
-return this.dataSource.filteredData.map(t => t.population).reduce((acc, value) => Number(acc) + Number(value), 0);
-  }
-  getTotalSqm() {
-    return this.dataSource.filteredData.map(t => t.areaInSqKm).reduce((acc, value) => Number(acc) + Number(value), 0);
-
-  }
-  
-
+getTotalPop() {
+const startIndex = this.pageIndex * this.pageSize;
+const endIndex = startIndex + this.pageSize;
+const itemsShowed = this.dataSource.filteredData.slice(startIndex, endIndex);
+return itemsShowed.map(t => t.population).reduce((acc, value) => Number(acc) + Number(value), 0);
+}
+getTotalSqm() {
+const startIndex = this.pageIndex * this.pageSize;
+const endIndex = startIndex + this.pageSize;
+const itemsShowed = this.dataSource.filteredData.slice(startIndex, endIndex);
+return itemsShowed.map(t => t.areaInSqKm).reduce((acc, value) => Number(acc) + Number(value), 0);
+}
 }
 
 
